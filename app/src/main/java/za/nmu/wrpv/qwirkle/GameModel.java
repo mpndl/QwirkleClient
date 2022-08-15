6c95d1bd0413ae22b1515a374db755ae62b51de5
git-en-dev <110345696+git-en-dev@users.noreply.github.com>
2022-08-15 13:26:07 +0200

    Fixed some bugs

--- app/src/main/java/za/nmu/wrpv/qwirkle/GameModel.java
+++ app/src/main/java/za/nmu/wrpv/qwirkle/GameModel.java
@@ -1,6 +1,8 @@
 package za.nmu.wrpv.qwirkle;
 
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,20 +19,30 @@ public class GameModel {
     private final String TAG = "game";
     private final int XLENGTH = 50;
     private final int YLENGTH = 50;
-    private ArrayList<Tile> plays = new ArrayList<>();
+    public ArrayList<Tile> places = new ArrayList<>();
     private int turns = 0;
+    private int tempTurns = -1;
+    private int points = 0;
     public Tile[][] board = new Tile[XLENGTH][YLENGTH];
+    public Tile[][] tempBoard = null;
+    private boolean placing = false;
+    public boolean backedup = false;
+    private TextView cPlayerTilesView;
+    private MainActivity mainActivity;
 
     public enum Legality {
         LEGAL, ILLEGAL;
     }
 
-    public GameModel(int pcount) {
+    public GameModel(int pcount, TextView cPlayerTilesView, MainActivity mainActivity) {
+        this.cPlayerTilesView = cPlayerTilesView;
+        this.mainActivity = mainActivity;
         PCOUNT = pcount;
         initializeTiles();
         initializePlayers();
         initialDraw();
         initialPlayer();
+        showCPlayerTiles();
     }
 
     private void initializeTiles() {
@@ -86,6 +98,15 @@ public class GameModel {
                 sCount = tempSCount;
                 cPlayer = player;
             }
+        }
+    }
+
+    public void showCPlayerTiles() {
+        int i = 0;
+        cPlayerTilesView.setText("");
+        for (Tile tile: cPlayer.tiles) {
+            cPlayerTilesView.setText(cPlayerTilesView.getText().toString() + "(" + tile.color + "," + tile.shape + ") ("+ i +"), ");
+            i++;
         }
     }
 
@@ -165,15 +186,43 @@ public class GameModel {
                 }
             }
             else {
-                for (int i = 0; i < tslength; i++) {
-                    boolean removed =  cPlayer.tiles.remove(ts[i]);
-                    if(removed) tiles.add(ts[i]);
+                for (Tile t : ts) {
+                    boolean removed = cPlayer.tiles.remove(t);
+                    if (removed) tiles.add(t);
                 }
                 Collections.shuffle(tiles);
                 for (int i = 0; i < tslength; i++) {
                     cPlayer.tiles.add(tiles.remove(i));
                 }
             }
+            if(placing)
+                turns = tempTurns;
+            placing = false;
+        }
+    }
+
+    public void draw(ArrayList<Tile> ts) {
+        int hcount = HCOUNT;
+        int tslength = ts.size();
+        if(tiles.size() > 0) {
+            if(ts.size() == 0) {
+                if(tiles.size() < 6) hcount = tiles.size();
+                for (int i = 0; i < hcount; i++) {
+                    if(cPlayer.tiles.size() < 6)
+                        cPlayer.tiles.add(tiles.remove(i));
+                }
+            }
+            else {
+                for (Tile t : ts) {
+                    boolean removed = cPlayer.tiles.remove(t);
+                    if (removed) tiles.add(t);
+                }
+                Collections.shuffle(tiles);
+                for (int i = 0; i < tslength; i++) {
+                    cPlayer.tiles.add(tiles.remove(i));
+                }
+            }
+            placing = false;
         }
     }
 
@@ -187,70 +236,168 @@ public class GameModel {
                 return;
             }
         }
+        turns++;
+        showCPlayerTiles();
     }
 
-    public Legality play(int xpos, int ypos, Tile tile) {
-        if (board[xpos][ypos] != null) return Legality.ILLEGAL;
-        if(legal(xpos, ypos, tile) == Legality.ILLEGAL) return Legality.ILLEGAL;
-        if(tile != null) {
+    public void place(int xpos, int ypos, Tile tile) {
+        Log.i(TAG, "place: " + tile.color + "," + tile.shape);
+        if(legal(xpos, ypos, tile) == Legality.LEGAL) {
+            if(!placing)
+                placing = true;
             tile.xPos = xpos;
             tile.yPos = ypos;
-            draw(tile);
-            board[xpos][ypos] = tile;
-            plays.add(tile);
-            turns++;
-            turn();
+            places.add(tile);
+            cPlayer.tiles.remove(tile);
+            tempBoard[tile.xPos][tile.yPos] = tile;
         }
-        return Legality.LEGAL;
+        else Toast.makeText(mainActivity, Legality.ILLEGAL + "", Toast.LENGTH_SHORT).show();
+        turns++;
+    }
+
+    public void recover() {
+        if(tempBoard != null) {
+            board = copy(tempBoard);
+        }
+    }
+
+    private Tile[][] copy(Tile[][] src) {
+        Tile[][] temp = new Tile[src.length][src[0].length];
+        for(int i=0; i<src.length; i++) {
+            System.arraycopy(src[i], 0, temp[i], 0, src[i].length);
+        }
+        return temp;
+    }
+
+    public void backup() {
+        tempBoard = copy(board);
+        tempTurns = turns;
+    }
+
+    public ArrayList<Tile> play() {
+        if(places.size() > 0) {
+            placing = false;
+            ArrayList<Tile> tiles = places;
+            assignPoints();
+            for (Tile tile: tiles) {
+                draw(tile);
+            }
+            //score();
+            return tiles;
+        }
+        return null;
     }
 
     private Legality legal(int xpos, int ypos, Tile tile) {
+        if (tempBoard[xpos][ypos] != null) return Legality.ILLEGAL;
         if(turns == 0)
             return Legality.LEGAL;
-        if (equivalent(xpos - 1, ypos, tile))
-            if(!duplicate(xpos, ypos, -1, 0, tile))
-                return Legality.LEGAL;
-        if (equivalent(xpos + 1, ypos, tile))
-            if(!duplicate(xpos, ypos, +1, 0, tile))
-                return Legality.LEGAL;
-        if (equivalent(xpos, ypos - 1, tile))
-            if(!duplicate(xpos, ypos, 0, -1, tile))
-                return Legality.LEGAL;
-        if (equivalent(xpos, ypos + 1, tile))
-            if(!duplicate(xpos, ypos, 0, +1, tile))
-                return Legality.LEGAL;
+        if (!duplicate(xpos, ypos, tile))
+            return Legality.LEGAL;
         return Legality.ILLEGAL;
     }
 
-    private boolean duplicate(int xpos, int ypos, int xdir, int ydir, Tile tile) {
-        ArrayList<Tile> tempTiles = new ArrayList<>();
-        int tempXpos = xpos + xdir;
-        int tempYpos = ypos + ydir;
-        while (withinBounds(tempXpos,tempYpos)) {
-            Tile tempTile = board[tempXpos][tempYpos];
-            if(tempTile == null) break;
-            tempTiles.add(tempTile);
-            tempXpos+=xdir;
-            tempYpos+=ydir;
-        }
-
-        if(tempTiles.size() >= 1) {
-            for (int i = 0; i < tempTiles.size(); i++) {
-                Tile tempTile = tempTiles.get(i);
-                if (tempTile.shape.equals(tile.shape) && tempTile.color.equals(tile.color))
-                    return true;
+    private boolean duplicate(int xpos, int ypos,Tile tile) {
+        if(equivalent(xpos - 1, ypos, tile) || !equivalent(xpos + 1, ypos, tile) ||
+                equivalent(xpos, ypos - 1, tile) || !equivalent(xpos, ypos + 1, tile)) {
+            ArrayList<Tile> tempTiles = new ArrayList<>();
+            for (Tile[] value : tempBoard) {
+                for (int j = 0; j < tempBoard.length; j++) {
+                    if (value[j] != null)
+                        tempTiles.add(value[j]);
+                }
             }
+            if(tempTiles.size() == 1) {
+                Tile tile2 = tempTiles.get(0);
+                if(tile2.color.equals(tile.color))
+                    return false;
+                else if(tile2.shape.equals(tile.shape))
+                    return false;
+            }
+            else if (tempTiles.size() >= 2) {
+                Tile tile1 = tempTiles.get(tempTiles.size() - 2);
+                Tile tile2 = tempTiles.get(tempTiles.size() - 1);
+                if(tile1.shape.equals(tile2.shape)) {
+                    if (tile1.yPos == tile2.yPos) {
+                        if (ypos == tile1.yPos) {
+                            if(tile.shape.equals(tile1.shape))
+                                return false;
+                        }
+                        else {
+                            if(xpos == tile1.xPos) {
+                                if(tile.color.equals(tile1.color))
+                                    return false;
+                            }
+                            else if(xpos == tile2.xPos) {
+                                if (tile.color.equals(tile2.color))
+                                    return false;
+                            }
+                        }
+                    } else if (tile1.xPos == tile2.xPos) {
+                        if(xpos == tile1.xPos) {
+                            if (tile.shape.equals(tile1.shape))
+                                return false;
+                        }
+                        else {
+                            if(ypos == tile1.yPos) {
+                                if (tile.color.equals(tile1.color))
+                                    return false;
+                            }
+                            else if (ypos == tile2.yPos) {
+                                if (tile.color.equals(tile2.color))
+                                    return false;
+                            }
+                        }
+                    }
+                }
+                else if(tile1.color.equals(tile2.color)) {
+                    if (tile1.yPos == tile2.yPos) {
+                        if (ypos == tile1.yPos) {
+                            if(tile.color.equals(tile1.color))
+                                return false;
+                        }
+                        else {
+                            if(xpos == tile1.xPos) {
+                                if(tile.shape.equals(tile1.shape))
+                                    return false;
+                            }
+                            else if(xpos == tile2.xPos) {
+                                if (tile.shape.equals(tile2.shape))
+                                    return false;
+                            }
+                        }
+                    } else if (tile1.xPos == tile2.xPos) {
+                        if(xpos == tile1.xPos) {
+                            if (tile.color.equals(tile1.color))
+                                return false;
+                        }
+                        else {
+                            if(ypos == tile1.yPos) {
+                                if (tile.shape.equals(tile1.shape))
+                                    return false;
+                            }
+                            else if (ypos == tile2.yPos) {
+                                if (tile.shape.equals(tile2.shape))
+                                    return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
 
-            if(tempTiles.size() >= 2) {
-                for (int i = 0; i < tempTiles.size(); i++) {
-                    Tile tile1 = tempTiles.get(tempTiles.size() - 2);
-                    Tile tile2 = tempTiles.get(tempTiles.size() - 1);
-                    if(tile1.color.equals(tile2.color))
-                        if(!tile2.color.equals(tile.color))
-                            return true;
-                    else if(tile1.shape.equals(tile2.shape))
-                        if (!tile2.shape.equals(tile.shape))
-                            return true;
+    private boolean equivalent(int xpos, int ypos, Tile tile1) {
+        if(xpos >= 0 && ypos >= 0 && xpos < XLENGTH && ypos < YLENGTH) {
+            Tile tile2 = tempBoard[xpos][ypos];
+            if(tile1 != null && tile2 != null) {
+                if(!(tile1.shape.equals(tile2.shape) && tile1.color.equals(tile2.color))) {
+                    if (tile1.shape.equals(tile2.shape)) {
+                        return true;
+                    } else {
+                        return tile1.color.equals(tile2.color);
+                    }
                 }
             }
         }
@@ -258,57 +405,32 @@ public class GameModel {
     }
 
     private boolean nul(int xpos, int ypos) {
-        if(xpos >= 0 && ypos >= 0 && xpos < XLENGTH && ypos < YLENGTH) {
-            Tile tile = board[xpos][ypos];
+        if(withinBounds(xpos, ypos)) {
+            Tile tile = tempBoard[xpos][ypos];
             return tile == null;
         }
         return true;
     }
 
     private boolean withinBounds(int xpos, int ypos) {
-        if(xpos >= 0 && ypos >= 0 && xpos < XLENGTH && ypos < YLENGTH)
-            return true;
-        return false;
-    }
-
-    private boolean equivalent(int xpos, int ypos, Tile tile1) {
-        if(xpos >= 0 && ypos >= 0 && xpos < XLENGTH && ypos < YLENGTH) {
-            Tile tile2 = board[xpos][ypos];
-            if(tile1 != null && tile2 != null) {
-                if (tile1.shape.equals(tile2.shape))
-                    return true;
-                else return tile1.color.equals(tile2.color);
-            }
-            return false;
-        }
-        return false;
+        return xpos >= 0 && ypos >= 0 && xpos < XLENGTH && ypos < YLENGTH;
     }
 
     private void assignPoints() {
-        for (Tile tile: plays) {
-            //calculate(tile.xPos, tile.yPos, -1, 0, tile);
-            calculate2(tile.xPos, tile.yPos, tile);
-        }
-        Log.i(TAG, "PLAYS: " + plays.size());
+        /*for (Tile tile: places) {
+            calculate(tile.xPos, tile.yPos, 0, 0);
+            // points--;
+            Log.i(TAG,  cPlayer.name+ " EARNED: >>>>> " + points);
+            Log.i(TAG, "assignPoints: ---------------------------------------");
+            cPlayer.points = cPlayer.points + points;
+            points = 0;
+        }*/
         // reinitialize
-        plays = new ArrayList<>();
-    }
-
-    private void calculate2(int xpos, int ypos, Tile tile) {
-        if (legal(xpos, ypos, tile) == Legality.LEGAL && withinBounds(xpos, ypos)) {
-            cPlayer.points++;
-            calculate2(xpos -1, ypos, tile);
-        }
-    }
-
-    private void calculate(int xpos, int ypos, int xdir, int ydir, Tile tile) {
-        if (legal(xpos, ypos, tile) == Legality.LEGAL && withinBounds(xpos, ypos)) {
-            cPlayer.points++;
-            calculate(xpos + xdir, ypos + ydir, xdir, ydir, tile);
-        }
+        places = new ArrayList<>();
     }
 
     private void score() {
+        Log.i(TAG, "---------------------------------------");
         for (Player player: players) {
             Log.i(TAG, player.name + ": " + player.points);
         }
--- app/src/main/java/za/nmu/wrpv/qwirkle/MainActivity.java
+++ app/src/main/java/za/nmu/wrpv/qwirkle/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,30 +19,55 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        gameModel = new GameModel(4);
+        setContentView(R.layout.activity_main);gameModel = new GameModel(4, findViewById(R.id.tiles), this);
 
     }
 
     public void setOnPlay(View view) {
-
-        EditText etXPos = findViewById(R.id.xPos);
-        EditText etYPos = findViewById(R.id.yPos);
-        TextView tvLegal = findViewById(R.id.legal);
-
-        int xPos = Integer.parseInt(etXPos.getText().toString());
-        int yPos = Integer.parseInt(etYPos.getText().toString());
-
-        Tile cPlayerTile = gameModel.cPlayer.tiles.get(0);
-        GameModel.Legality legality = gameModel.play(xPos, yPos, cPlayerTile);
-        if(legality == GameModel.Legality.LEGAL) {
-            tvLegal.setText(tvLegal.getText().toString() + "(" + gameModel.board[xPos][yPos].color + "," + gameModel.board[xPos][yPos].shape + "), ");
+        if(gameModel.places.size() > 0) {
+            TextView tvLegal = findViewById(R.id.legal);
+            gameModel.recover();
+            ArrayList<Tile> tiles = gameModel.play();
+            if (tiles != null)
+                for (Tile tile : tiles)
+                    tvLegal.setText(tvLegal.getText().toString() + "(" + tile.color + "," + tile.shape + ")(" + tile.xPos + "," + tile.yPos + "), ");
+            gameModel.turn();
+            gameModel.showCPlayerTiles();
         }
     }
 
     public void setOnDraw(View view) {
-        gameModel.draw(gameModel.cPlayer.tiles.get(0), gameModel.cPlayer.tiles.get(1), gameModel.cPlayer.tiles.get(2), gameModel.cPlayer.tiles.get(3), gameModel.cPlayer.tiles.get(4), gameModel.cPlayer.tiles.get(5));
+        gameModel.draw(addAll());
+        if (gameModel.places.size() == 0)
+            gameModel.recover();
         gameModel.turn();
+        gameModel.showCPlayerTiles();
+    }
+
+    private ArrayList<Tile> addAll() {
+        ArrayList<Tile> ts = new ArrayList<>();
+        ts.addAll(gameModel.cPlayer.tiles);
+        return ts;
+    }
+
+    public void setOnPlace(View view) {
+        EditText etXPos = findViewById(R.id.xPos);
+        EditText etYPos = findViewById(R.id.yPos);
+        EditText etIndex = findViewById(R.id.index);
+        int xPos = Integer.parseInt(etXPos.getText().toString());
+        int yPos = Integer.parseInt(etYPos.getText().toString());
+        int index = Integer.parseInt(etIndex.getText().toString());
+        if(!gameModel.backedup) {
+            gameModel.backedup = true;
+            gameModel.backup();
+        }
+        gameModel.place(xPos, yPos, get(index));
+        gameModel.showCPlayerTiles();
+    }
+
+    private Tile get(int i) {
+        if (i >= 0 && i < gameModel.cPlayer.tiles.size())
+            return gameModel.cPlayer.tiles.get(i);
+        return null;
     }
 }
